733fa4f759ae64d771819ecd2a6e21c3a47a6b0a
christian.buele <christian.buele@ucuenca.edu.ec>
2025-06-18 21:15:54 -0500

    Update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
@@ -13,22 +13,34 @@ public class Main {
     public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
 
         if (args.length != 2) {
-            System.err.println("Uso: MediaGolesDriver <input> <output>");
+            System.err.println("Uso: MediaGolesPartido <input path> <output path>");
             System.exit(-1);
         }
 
         Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "media goles por partido");
+        Job job = Job.getInstance(conf, "Media de Goles por Partido");
+
         job.setJarByClass(Main.class);
 
+        // Clases de Mapper, Combiner y Reducer
         job.setMapperClass(Mapper.class);
+        job.setCombinerClass(CombinerMediaGoles.class);
         job.setReducerClass(ReducerMediaGoles.class);
 
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+        // Tipos de salida del Mapper
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
 
+        // Tipos de salida final
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(DoubleWritable.class);
+
+        // Archivos de entrada/salida
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        // Salida ordenada alfabéticamente
+        job.setSortComparatorClass(Text.Comparator.class);
 
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
